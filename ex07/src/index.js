387d43ac0218f2f919e2fb169058ce5c6ad0c8dd
benjamindz <dzaferovicbenjamin@gmail.com>
2021-03-20 02:58:02 +0100

    made changes to the file

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,41 +1,42 @@
 var collection = {
-  2548: {
-    album: "Slippery When Wet",
-    artist: "Bon Jovi",
-    tracks: ["let It Rock", "You Give Love a Bad Name"],
-  },
-  2468: {
-    album: "1999",
-    artist: "Prince",
-    tracks: ["1999", "Little Red Corvette"],
-  },
-  1245: {
-    artist: "Robert Palmer",
-    tracks: [],
-  },
-  5439: {
-    album: "ABBA Gold",
-  },
-};
-
-var oldCollection = JSON.parse(JSON.stringify(collection));
-
-function updateRecords(object, id, prop, value) {
-  if (object[id][prop] !== "tracks" && object[id][prop] !== "") {
-    object[id][prop] = value;
-  } else if (prop == "tracks" && object.id.hasOwnProperty("tracks") == fasle) {
-    object[id][prop] = [];
-    object[id][prop].push(value);
-  } else if (object[id][prop] == "tracks" && object[id][prop] !== "") {
-    object[id][prop].push(value);
-  } else if (object[id][prop] == "") {
-    delete object[id][prop];
+    2548: {
+      album: "Slippery When Wet",
+      artist: "Bon Jovi",
+      tracks: ["let It Rock", "You Give Love a Bad Name"],
+    },
+    2468: {
+      album: "1999",
+      artist: "Prince",
+      tracks: ["1999", "Little Red Corvette"],
+    },
+    1245: {
+      artist: "Robert Palmer",
+      tracks: [],
+    },
+    5439: {
+      album: "ABBA Gold",
+    },
+  };
+  
+  var oldCollection = JSON.parse(JSON.stringify(collection));
+  
+  function updateRecords(object, id, prop, value) {
+    if (prop !== "tracks" && prop != "") {
+      object[id][prop] = value;
+    } else if (prop == "tracks" && object[id].hasOwnProperty("tracks") != true) {
+      object[id][prop] = [];
+      object[id][prop].push(value);
+    } else if (prop == "tracks" && value != "") {
+      object[id][prop].push(value);
+    } else if (value == "") {
+      delete object[id][prop];
+    }
+    return object;
   }
-  return object;
-}
-
-console.log(updateRecords(collection, 5439, "artist", "ABBA"));
-console.log(updateRecords(collection, 5439, "tracks", "Take a Chance on Me"));
-console.log(updateRecords(collection, 2548, "artist", ""));
-console.log(updateRecords(collection, 1245, "tracks", "Addicted to Love"));
-module.exports = updateRecords;
+  
+  console.log(updateRecords(collection, 5439, "artist", "ABBA"));
+  console.log(updateRecords(collection, 5439, "tracks", "Take a Chance on Me"));
+  console.log(updateRecords(collection, 2548, "artist", ""));
+  console.log(updateRecords(collection, 1245, "tracks", "Addicted to Love"));
+  module.exports = updateRecords;
+  
